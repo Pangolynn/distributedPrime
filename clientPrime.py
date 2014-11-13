f061a24bd77fe19aeab17ef9cd4897fb54ec6751
Sammilynn <hollow71@students.rowan.edu>
2014-11-13 08:48:34 -0500

    print statementsfixed to python3 compliant

--- clientPrime.py
+++ clientPrime.py
@@ -1,13 +1,14 @@
 # Joseph Pannizzo
 # RPYC Client
+#edited by Samantha Holloway 11/14
 
 import rpyc
 
 def sentPrime(input): 
     # host needs to be changed for remote access
-    conn = rpyc.connect("127.0.0.1", 12345) # connect to the server on my local machine for testing. 
-    print conn # print connection
-    print conn.root.return_primes(input) # send input to return primes, then print
+    conn = rpyc.connect("192.168.0.112", 12345) # connect to the server on my local machine for testing.
+    print(conn) # print connection
+    print(conn.root.return_primes(input)) # send input to return primes, then print
     
 # --Start Program--
 input = int(input("Enter a number: ")) # ask user for number
